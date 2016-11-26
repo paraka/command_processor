547ee014c8cc67b3aa4f947ce1453dabec54b6eb
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2016-11-26 11:40:00 +0100

    Main example of how to use this code

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "CommandParser.h"
+#include "CommandProcessor.h"
+
+int main(int argc, char **argv)
+{
+    CommandProcessor cmd_processor;
+
+    char data[3] = {0x01, 0x03, 0x04 };
+
+    cmd_processor.process(CommandParser::read_command_from_data(data), "Parameter for A command");
+
+    data[0] = 0x02;
+
+    cmd_processor.process(CommandParser::read_command_from_data(data), "Parameter for B command");
+
+    return 0;
+}
